18fccfc60f30200d4033fb3d579b3f3fc8dc7816
Renky32 <s160420066@student.ubaya.ac.id>
2024-04-22 10:55:47 +0700

    init

--- app/src/main/java/com/example/project160420055/view/ProfilFragment.kt
+++ app/src/main/java/com/example/project160420055/view/ProfilFragment.kt
@@ -6,8 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.project160420055.R
+import com.example.project160420055.databinding.FragmentDetailBinding
+import com.example.project160420055.databinding.FragmentProfilBinding
+import com.example.project160420055.viewmodel.DetailViewModel
+import com.example.project160420055.viewmodel.LoginViewModel
 
 class ProfilFragment : Fragment() {
+    private lateinit var viewmodel: LoginViewModel
+    private lateinit var binding: FragmentProfilBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,10 @@ class ProfilFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
